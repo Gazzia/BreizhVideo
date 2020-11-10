1243d2b38b8fbe68b390052f7d5ff2ca2cc58543
Joseph Allain <36985411+Gazzia29@users.noreply.github.com>
2020-11-10 15:42:52 +0100

    Correction

--- security.sql
+++ security.sql
@@ -3,22 +3,24 @@ CREATE ROLE
     breizh_viewer;
     
 GRANT SELECT 
-ON breizhvideo.* 
+ON breizhvideo.*
 TO breizh_staff, breizh_viewer;
 
 GRANT INSERT, UPDATE, DELETE
-ON breizhvideo.* 
+ON breizhvideo.*
 TO breizh_staff;
     
 CREATE USER staff1 IDENTIFIED BY 'staff1';   
 CREATE USER viewer1 IDENTIFIED BY 'viewer1'; 
-GRANT breizh_staff, 
-   breizh_viewer
-TO staff1, 
-    viewer1;
+
+GRANT breizh_viewer
+TO viewer1;
+    
+GRANT breizh_staff
+TO staff1;
     
 GRANT INSERT, UPDATE
 ON breizhvideo.preference
 TO viewer1;
 
-SET DEFAULT ROLE ALL TO breiz_viewer;
+SET DEFAULT ROLE ALL TO breizh_viewer;
